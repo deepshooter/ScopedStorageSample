08265b5a02466394df2fb8e1832ef9bd2995dab0
deepshooter <avinashm720@gmail.com>
2024-04-23 11:56:45 +0530

    Add InternalStorage SavePhotos , LoadPhotos , DeletePhotos functions

--- app/src/main/java/com/deepshooter/scopedstoragesample/MainActivity.kt
+++ app/src/main/java/com/deepshooter/scopedstoragesample/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.deepshooter.scopedstoragesample
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.deepshooter.scopedstoragesample.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
+
+
+    private fun deletePhotosFromInternalStorage(fileName: String): Boolean {
+        return try {
+            deleteFile(fileName)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    private suspend fun loadPhotosFromInternalStorage(): List<InternalStoragePhoto> {
+        return withContext(Dispatchers.IO) {
+            val files = filesDir.listFiles()
+            files?.filter { it.canRead() && it.isFile && it.endsWith(".jpg") }?.map {
+                val bytes = it.readBytes()
+                val bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                InternalStoragePhoto(it.name, bmp)
+            }
+        } ?: listOf()
+    }
+
+    private fun savePhotoToInternalStorage(fileName: String, bmp: Bitmap): Boolean {
+
+        return try {
+            openFileOutput("$fileName.jpg", MODE_PRIVATE).use { stream ->
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 95, stream)) {
+                    throw IOException("Couldn't Save Bitmap.")
+                }
+            }
+            true
+        } catch (e: IOException) {
+            e.printStackTrace()
+            false
+        }
+    }
+
 }
